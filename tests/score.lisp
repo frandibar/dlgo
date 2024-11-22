64d17d61367fc4b2d47bae338f1a58a1ea21bd1a
Francisco Dibar <frandibar@gmail.com>
2024-11-22 19:28:31 -0300

    Add test

--- tests/score.lisp
+++ tests/score.lisp
@@ -58,3 +58,39 @@
     (is (= (dlgo.score::score-dame score) 3))
     (is (= 7.5 (dlgo:game-komi (game-info game))))
     (is (string= (dlgo.score:game-result game) "W+11.5"))))
+
+(test test-score-2 ()
+  ;; 7 . ○ ○ ● . ● .
+  ;; 6 ○ . ○ ● ● ● ●
+  ;; 5 ○ ○ ○ ○ ○ ○ ○
+  ;; 4 ○ ● ● ○ . . .
+  ;; 3 ● ● . ● ○ . .
+  ;; 2 . ● . ● ○ . .
+  ;; 1 . . ● ○ ○ . .
+  ;;   A B C D E F G
+  (let* ((game (do-moves (make-game 7)
+		 A4 A3
+		 A5 B2
+		 A6 B3
+		 B5 B4
+		 B7 C1
+		 C5 C4
+		 C6 D2
+		 C7 D3
+		 D1 D6
+		 D4 D7
+		 D5 E6
+		 E1 F6
+		 E2 F7
+		 E3 G6
+		 E5 'pass
+		 F5 'pass
+		 G5 'pass))
+	 (score (dlgo.score:score-game game)))
+    (is (= (dlgo.score::score-black-stones score) 17))
+    (is (= (dlgo.score::score-black-territory score) 11))
+    (is (= (dlgo.score::score-white-stones score) 14))
+    (is (= (dlgo.score::score-white-territory score) 7))
+    (is (= (dlgo.score::score-dame score) 0))
+    (is (= 7.5 (dlgo:game-komi (game-info game))))
+    (is (string= (dlgo.score:game-result game) "W+0.5"))))
